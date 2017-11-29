aacc80d62e79988598d4faa51e5b8fd5d74b3d61
Dhen Padilla <dhenepadilla@gmail.com>
2017-11-29 15:16:44 +0000

    Maven project config

--- oyster/src/test/java/com/tfl/billing/CostCalculatorTest.java
+++ oyster/src/test/java/com/tfl/billing/CostCalculatorTest.java
@@ -1,14 +1,43 @@
 package com.tfl.billing;
 
+import static org.mockito.Mockito.*;
+
+import com.oyster.OysterCard;
+import com.oyster.ScanListener;
+import com.tfl.external.Customer;
+import com.tfl.external.CustomerDatabase;
+
+import com.tfl.underground.OysterReaderLocator;
+import com.tfl.underground.Station;
 import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
 
 import static org.junit.Assert.*;
 
 public class CostCalculatorTest {
 
-    @Test
-    public void generatesCorrectTotal() {
+   TravelTracker tracker = new TravelTracker();
 
-    }
+   // @Rule
+   OysterCardReader mockedReader = mock(OysterCardReader.class);
+   List<Customer> customers = CustomerDatabase.getInstance().getCustomers();
+
+   @Test
+   public void correctListOfReaderScans() {
+       UUID readerId = mockedReader.getReaderId();
+       tracker.cardScanned(customers.get(0).cardId(), readerId);
+
+   }
+
+   /*@Test
+   public void generatesCorrectTotal() {
+       Customer cust1 = new Customer("John Smith", new OysterCard());
+       tracker.connect();
+       tracker.cardScanned(cust1.cardId(), mockedReader.getReaderId());
+       //assertEquals(cust1.cardId(), tracker.);
+   }*/
 
 }
